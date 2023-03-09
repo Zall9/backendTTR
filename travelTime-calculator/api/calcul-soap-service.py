78a026627217f0b662ab1ecccfec2d7d44f5e3a9
Paul Delifer <paul.delifer@yahoo.fr>
2023-03-09 12:58:22 +0100

    soap

--- travelTime-calculator/api/calcul-soap-service.py
+++ travelTime-calculator/api/calcul-soap-service.py
@@ -13,7 +13,7 @@ class TrajetService(ServiceBase):
     origin = '*'
 
     @rpc(float, float, float, float, float, float, float, _returns=float)
-    def calculer_temps_trajetV2(ctx, vitesse_moyenne, lat1, lon1, lat2, lon2, autonomie, temps_recharge_min):
+    def calculer_temps_trajet(ctx, vitesse_moyenne, lat1, lon1, lat2, lon2, autonomie, temps_recharge_min):
         ctx.transport.resp_headers['Access-Control-Allow-Origin'] = '*'
 
         # Utiliser GeoPy pour calculer la distance entre les coordonnées GPS
@@ -31,24 +31,6 @@ class TrajetService(ServiceBase):
         temps_trajet += temps_recharge_total
         # Retourner le temps de trajet
         return (temps_trajet / 10) * 2
-
-    @rpc(Decimal, Decimal, Decimal, Decimal, _returns=Decimal)
-    def calculer_temps_trajet(ctx, distance_km, vitesse_km_h, autonomie_km, temps_recharge_h):
-        ctx.transport.resp_headers['Access-Control-Allow-Origin'] = ctx.descriptor.service_class.origin
-        temps_trajet = distance_km / vitesse_km_h  # Temps de trajet sans recharge
-        # Nombre de recharges nécessaires
-        nb_recharges = int(math.ceil(distance_km / autonomie_km)) - 1
-        temps_recharge_total = nb_recharges * \
-            temps_recharge_h  # Temps total de recharge
-        # Temps total de trajet avec recharges
-        temps_trajet_total = temps_trajet + temps_recharge_total
-        return temps_trajet_total
-
-
-
-
-def dist(lat1, lon1, lat2, lon2):
-    return distance.distance((lat1, lon1), (lat2, lon2)).km
 
 application = Application([TrajetService], 'mon_app_serveur.soap',
                           in_protocol=Soap11(validator='lxml'),
